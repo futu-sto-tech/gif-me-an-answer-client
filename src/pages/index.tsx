91442ea063682f8ddb4418da953149e38746419e
Robin Andeer <robin.andeer@gmail.com>
2020-12-16 20:00:42 +0100

    Integrate styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`;
+
 const IndexPage: React.FC = () => {
-  return <h1 className="text-black dark:text-white">It&apos;s game time!</h1>;
+  return (
+    <>
+      <h1 className="text-black dark:text-white">It&apos;s game time!</h1>
+      <Title>Join and let&apos;s play!</Title>
+    </>
+  );
 };
 
 export default IndexPage;
