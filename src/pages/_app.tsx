8b6bd2986cc95d012ed5ede1cb276fba0678edd6
Viktorija Valso <viktorijavalsoe@gmail.com>
2020-12-17 19:31:54 +0100

    add colors, images and svs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
 
 import { AppProps } from 'next/app';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      pink: string;
+      darkPink: string;
+      cyan: string;
+      black: string;
+      white: string;
+      background: string;
+    };
+  }
+}
+
+const theme: DefaultTheme = {
+  colors: {
+    pink: '#DD01A1',
+    darkPink: '#8D0066',
+    cyan: '#00EFFF',
+    black: '#000',
+    white: '#FFFFF',
+    background: '#2D2D2D',
+  },
+};
+
+export default theme;
