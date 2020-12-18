cd7331c5f80a817385f8966d534fce81e40f8089
Robin Andeer <robin.andeer@gmail.com>
2020-12-18 09:34:24 +0100

    Bootstrap routes

---
+++ src/pages/game/index.tsx
@@ -0,0 +1,5 @@
+const GamePage: React.FC = () => {
+  return <h1 className="text-black dark:text-white">Join a game!</h1>;
+};
+
+export default GamePage;
---
+++ src/pages/game/new.tsx
@@ -0,0 +1,5 @@
+const GameNewPage: React.FC = () => {
+  return <h1 className="text-black dark:text-white">So you want to create a new game?</h1>;
+};
+
+export default GameNewPage;
