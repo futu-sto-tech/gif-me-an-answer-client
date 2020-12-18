80aa68418fd8b9eb2e47748e9fdfae6939a44fa7
Robin Andeer <robin.andeer@gmail.com>
2020-12-18 14:33:47 +0100

    Add browse screen

--- src/screens/browse.tsx
+++ src/screens/browse.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Dialog } from '@reach/dialog';
+import Button from 'components/Button';
 
 const BrowseScreen: React.FC<{ game: { players: [] } }> = () => {
   const [query, setQuery] = useState('');
   const [image, setImage] = useState<string | null>(null);
 
+  const [secondsLeft, setSecondsLeft] = useState(60 * 2);
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      setSecondsLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(handle);
+  });
+
   return (
     <div className="flex flex-col items-center p-12 space-y-6">
       <div className="flex items-center justify-center w-24 h-24 text-2xl font-bold text-black bg-white rounded-full">
-        1:59
+        {Math.floor(secondsLeft / 60)}:{secondsLeft - 60 * Math.floor(secondsLeft / 60)}
       </div>
       <div className="max-w-xl space-y-6">
         <p className="text-xl font-bold text-center text-white">
@@ -33,12 +44,12 @@ const BrowseScreen: React.FC<{ game: { players: [] } }> = () => {
       </div>
 
       <Dialog
-        className="flex flex-col items-center shadow-2xl bg-background rounded-xl"
+        className="flex flex-col items-center space-y-8 shadow-2xl bg-background rounded-xl"
         isOpen={image !== null}
         onDismiss={() => setImage(null)}
       >
         {image && <img src={image} className="border-4 border-black" />}
-        <button>Vote for GIF</button>
+        <Button type="button" buttonText="Vote for GIF" />
       </Dialog>
     </div>
   );
