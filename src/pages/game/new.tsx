d682d1c4119aa53c14a6ad8acf3726dac2a30487
Deniz Eroglu <denizeroglu@me.com>
2020-12-18 16:24:59 +0100

    Connect create game to new game page

--- src/pages/game/new.tsx
+++ src/pages/game/new.tsx
@@ -1,10 +1,22 @@
+import API from 'api';
 import MainLayout from 'components/MainLayout';
 import PageTitle from 'components/PageTitle';
+import { useEffect, useState } from 'react';
+import { Game } from 'types';
 
 const GameNewPage: React.FC = () => {
+  const [game, setGame] = useState<Game>();
+  useEffect(() => {
+    API.createGame(4).then(function (newGame) {
+      console.log(newGame);
+      setGame(newGame);
+    });
+  }, []);
+
   return (
     <MainLayout>
       <PageTitle>Create a new game</PageTitle>
+      {game == undefined ? <p> Creating game</p> : <p>Code is {game?.code}</p>}
     </MainLayout>
   );
 };
