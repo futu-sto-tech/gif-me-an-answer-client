e37fe0e24b207c87cddfbfaf8b36bec35442c969
Robin Andeer <robin.andeer@gmail.com>
2020-12-19 15:53:16 +0100

    Integrate event source

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,14 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+
+import BrowseScreen from 'screens/browse';
 import { DUMMY_GAME_DATA } from 'fixtures';
+import { Game } from 'types';
 import JoinGameScreen from 'screens/join-game';
 import LobbyScreen from 'screens/lobby';
-import { useState } from 'react';
+
+function useEventSource<T>(url: string | null): T | null {
+  const events = useRef<EventSource | null>();
+  const [data, setData] = useState<T | null>(null);
+
+  useEffect(() => {
+    if (url) {
+      events.current = new EventSource(url);
+      events.current.addEventListener('message', (event) => {
+        console.log(event);
+        setData(event.data);
+      });
+
+      return () => events.current?.close();
+    }
+
+    return;
+  }, [url]);
+
+  return data;
+}
 
 const GamePage: React.FC = () => {
   const [code, setCode] = useState<number | null>(null);
+  const game = useEventSource<Game>(code ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${code}/event` : null);
 
   const handleGameSetup = (gameCode: number) => {
     setCode(gameCode);
   };
+
+  if (game && game.rounds.length > 0) {
+    return <BrowseScreen game={DUMMY_GAME_DATA} />;
+  }
 
   if (code) {
     return <LobbyScreen game={DUMMY_GAME_DATA} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import API from 'api';
 import LinkButton from 'components/LinkButton';
 import styled from 'styled-components';
 
@@ -11,7 +10,7 @@ const IndexPage: React.FC = () => {
   return (
     <MainContainer className="flex flex-col items-center justify-center h-screen">
       <img src="/assets/bongocat-1.png" alt="moscot cat" />
-      <img src="/assets/logo.png" alt="logo" className="mb-9" />
+      <img src="/assets/Logo.png" alt="logo" className="mb-9" />
       <div className="grid gap-8 lg:grid-cols-2">
         <LinkButton linkText="Join game" href="/game/" />
         <LinkButton linkText="Host game" href="/game/new" />
