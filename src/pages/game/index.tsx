80e1e876de2e7f34b5a9d83be79ea93645ce5498
Robin Andeer <robin.andeer@gmail.com>
2020-12-19 19:24:23 +0100

    Update game logic
    Extract hook.

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,42 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import { DUMMY_GAME_DATA, DUMMY_PLAYER_DATA } from 'fixtures';
+import { Game, GameRoundStatus } from 'types';
+import { useMemo, useState } from 'react';
 
 import BrowseScreen from 'screens/browse';
-import { DUMMY_GAME_DATA } from 'fixtures';
-import { Game } from 'types';
+import FinalResultsScreen from 'screens/final-results';
+import FinalWinnerScreen from 'screens/final-winner';
 import JoinGameScreen from 'screens/join-game';
 import LobbyScreen from 'screens/lobby';
-
-function useEventSource<T>(url: string | null): T | null {
-  const events = useRef<EventSource | null>();
-  const [data, setData] = useState<T | null>(null);
-
-  useEffect(() => {
-    if (url) {
-      events.current = new EventSource(url);
-      events.current.addEventListener('message', (event) => {
-        console.log(event);
-        setData(event.data);
-      });
-
-      return () => events.current?.close();
-    }
-
-    return;
-  }, [url]);
-
-  return data;
-}
+import { useEventSource } from 'hooks';
 
 const GamePage: React.FC = () => {
-  const [code, setCode] = useState<number | null>(null);
+  const [code, setCode] = useState<number | null>(7856);
   const game = useEventSource<Game>(code ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/games/${code}/event` : null);
 
   const handleGameSetup = (gameCode: number) => {
     setCode(gameCode);
   };
 
-  if (game && game.rounds.length > 0) {
-    return <BrowseScreen game={DUMMY_GAME_DATA} />;
+  const latestRound = useMemo(() => game?.rounds[game.rounds.length - 1], [game?.rounds]);
+
+  const gameOver = useMemo(() => {
+    if (game) {
+      const allRounds = game.rounds.length === game.totalRounds;
+      const allFinished = game.rounds.every((item) => item.status === GameRoundStatus.FINSIHED);
+      return allRounds && allFinished;
+    }
+  }, [game]);
+
+  if (gameOver) {
+    return <FinalWinnerScreen game={DUMMY_GAME_DATA} player={DUMMY_PLAYER_DATA} />;
+  }
+
+  if (latestRound) {
+    switch (latestRound.status) {
+      case GameRoundStatus.SELECT_GIF:
+        return <BrowseScreen game={DUMMY_GAME_DATA} />;
+      case GameRoundStatus.VOTE:
+        return null;
+      case GameRoundStatus.PRESENT:
+        return null;
+      case GameRoundStatus.FINSIHED:
+        return <FinalResultsScreen game={DUMMY_GAME_DATA} player={DUMMY_PLAYER_DATA} />;
+    }
   }
 
   if (code) {
