1445da3a7d710f43d063a9a46a744980c68a5085
Robin Andeer <robin.andeer@gmail.com>
2020-12-20 22:26:02 +0100

    Show selected GIF in modal
    Add game fixtures for testing.

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,4 +1,4 @@
-import { Game, GameRoundStatus, GameStatus, Player, PlayerStatus } from 'types';
+import { Game, GameRound, GameRoundStatus, GameStatus, Image, Player, PlayerStatus } from 'types';
 
 export const DUMMY_PLAYER_DATA: Player = { id: 'player1', name: 'Hi', points: 22, status: PlayerStatus.READY };
 
@@ -16,4 +16,53 @@ export const DUMMY_GAME_DATA: Game = {
       images: [],
     },
   ],
+};
+
+export const BASE_PLAYER: Player = {
+  id: 'player1',
+  name: 'Playya',
+  points: 0,
+  status: PlayerStatus.JOINED,
+};
+
+const BASE_GAME: Game = {
+  code: '7856',
+  players: [],
+  status: GameStatus.ACTIVE,
+  totalRounds: 8,
+  rounds: [],
+};
+
+const BASE_ROUND: GameRound = {
+  order: 1,
+  status: GameRoundStatus.SELECT_GIF,
+  caption: "What a day I've had...",
+  presentImage: '',
+  images: [],
+};
+
+const BASE_IMAGE: Image = {
+  id: 'image1',
+  url: 'https://media.giphy.com/media/lXiRKBj0SAA0EWvbG/giphy.gif',
+  playerId: BASE_PLAYER.id,
+  votes: 0,
+};
+
+export const GAME_NOT_STARTED = null;
+
+export const GAME_STARTED: Game = BASE_GAME;
+
+export const GAME_JOINED: Game = {
+  ...BASE_GAME,
+  players: [BASE_PLAYER],
+};
+
+export const GAME_ROUND_ONE: Game = {
+  ...GAME_JOINED,
+  rounds: [BASE_ROUND],
+};
+
+export const GAME_ROUND_SELECTED: Game = {
+  ...GAME_ROUND_ONE,
+  rounds: [{ ...GAME_ROUND_ONE.rounds[0], images: [BASE_IMAGE] }],
 };
--- src/screens/browse.tsx
+++ src/screens/browse.tsx
@@ -1,5 +1,5 @@
 import { Game, GameRound, Gif, Player } from 'types';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import API from 'api';
 import Button from 'components/Button';
@@ -38,12 +38,14 @@ const BrowseScreen: React.FC<ScreenProps> = ({ game, round, player }) => {
     await API.submitGif({ code: game.code, order: round.order, player, gifUrl: url });
   };
 
+  const playerImage = useMemo(() => round.images.find((item) => item.playerId === player.id), [round, player]);
+
   return (
     <div className="flex flex-col items-center p-12 space-y-6">
       <div className="flex items-center justify-center w-24 h-24 text-2xl font-bold text-black bg-white rounded-full">
         {Math.floor(secondsLeft / 60)}:{secondsLeft - 60 * Math.floor(secondsLeft / 60)}
       </div>
-      <div className="max-w-xl space-y-6">
+      <div className="w-full max-w-xl space-y-6">
         <p className="text-xl font-bold text-center text-white">{round.caption} </p>
         <input
           className="w-full px-4 py-2 text-2xl font-bold border-2 border-black rounded text-pink"
@@ -66,19 +68,30 @@ const BrowseScreen: React.FC<ScreenProps> = ({ game, round, player }) => {
         })}
       </div>
 
-      <Dialog
-        aria-label="selected gif"
-        className="flex flex-col items-center space-y-8 shadow-2xl bg-background rounded-xl"
-        isOpen={image !== null}
-        onDismiss={() => setImage(null)}
-      >
-        {image && (
-          <>
-            <img src={image} className="border-4 border-black" />
-            <Button type="button" buttonText="Select GIF" handleClick={() => handleSubmitGif(image)} />
-          </>
-        )}
-      </Dialog>
+      {playerImage ? (
+        <Dialog
+          aria-label="selected gif"
+          className="flex flex-col items-center space-y-8 shadow-2xl bg-background rounded-xl"
+          isOpen={true}
+        >
+          <img src={playerImage.url} className="border-4 border-black" />
+          <Button type="button" buttonText="Selected GIF" />
+        </Dialog>
+      ) : (
+        <Dialog
+          aria-label="selected gif"
+          className="flex flex-col items-center space-y-8 shadow-2xl bg-background rounded-xl"
+          isOpen={image !== null}
+          onDismiss={() => setImage(null)}
+        >
+          {image && (
+            <>
+              <img src={image} className="border-4 border-black" />
+              <Button type="button" buttonText="Select GIF" handleClick={() => handleSubmitGif(image)} />
+            </>
+          )}
+        </Dialog>
+      )}
     </div>
   );
 };
