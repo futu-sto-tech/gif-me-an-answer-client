cb764aec5733b0ea4091bb469da984db25c1880f
Robin Andeer <robin.andeer@gmail.com>
2021-01-08 12:13:11 +0100

    Remove unused screens

--- src/pages/result/index.tsx
+++
@@ -1,9 +0,0 @@
-import { DUMMY_GAME_DATA, DUMMY_PLAYER_DATA } from 'fixtures';
-
-import FinalResultsScreen from '../../screens/final-results';
-
-const ResultPage: React.FC = () => {
-  return <FinalResultsScreen game={DUMMY_GAME_DATA} player={DUMMY_PLAYER_DATA} />;
-};
-
-export default ResultPage;
