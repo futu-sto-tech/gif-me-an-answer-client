ba0fbe0af7e13eb6d2f0365be81b0f6e8bfaceaf
Robin Andeer <robin.andeer@gmail.com>
2021-03-11 20:09:22 +0100

    Use custom font
    Tweak present screen layout.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 
 import { ServerStyleSheet } from 'styled-components';
 
@@ -26,5 +26,20 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- src/screens/present.tsx
+++ src/screens/present.tsx
@@ -2,7 +2,7 @@ import { RoundScreenProps } from 'types';
 
 const PresentScreen: React.FC<RoundScreenProps> = ({ round }) => {
   return (
-    <div className="space-y-12">
+    <div className="space-y-12 py-6 md:py-12">
       <p className="text-2xl font-bold text-center text-white">{round.caption}</p>
       <img src={round.presentImage || '/assets/bongocat-2.png'} />
     </div>
