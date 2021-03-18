af95e345d434d58edbe1f9f208fc92bf32e240d3
Robin Andeer <robin.andeer@gmail.com>
2021-03-18 23:12:41 +0100

    Add exit game button to navigation
    Fix layout issues.

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,56 +3,54 @@ import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import * as types from 'types';
 
-const Navigation = styled.nav`
-  height: 80px;
-  padding: 24px;
-  a {
-    img {
-      height: 70px;
-    }
-  }
-`;
-
-const MainContainer = styled.main`
-  width: 90%;
-  margin: 0 auto;
-  flex: 1;
+const Container = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 88px 1fr;
 `;
 
 interface IMainLayoutProps {
   currentRound?: types.GameRound;
   totalRounds?: number;
+  onExit: () => void;
 }
 
-const MainLayout: React.FC<IMainLayoutProps> = ({ children, currentRound, totalRounds }) => {
+const MainLayout: React.FC<IMainLayoutProps> = ({ children, currentRound, totalRounds, onExit }) => {
   const { pathname } = useRouter();
 
   return (
-    <div className="flex flex-col h-screen">
+    <Container>
       {pathname !== '/' ? (
-        <Navigation className="flex space-x-4">
-          <Link href="/">
-            <a>
-              <img src="/assets/Logo.png" style={{ height: 64 }} alt="logo" />
-            </a>
-          </Link>
-          {currentRound ? (
-            <div className="relative" style={{ width: 55, height: 55 }}>
-              <img src="/assets/round.svg" height="55" width="55" alt="logo" />
-              <div className="absolute inset-0 flex items-center justify-center flex-col space-y-1">
-                <p className="text-white text-center leading-none -mb-px" style={{ fontSize: 9 }}>
-                  Round
-                </p>
-                <p className="text-center text-white text-sm leading-none">
-                  {currentRound.order}/{totalRounds}
-                </p>
+        <nav className="flex items-center h-full justify-between p-4">
+          <div className="flex space-x-4 flex-shrink-0">
+            <Link href="/">
+              <a>
+                <img src="/assets/Logo.png" style={{ height: 64 }} alt="logo" />
+              </a>
+            </Link>
+            {currentRound ? (
+              <div className="relative" style={{ width: 55, height: 55 }}>
+                <img src="/assets/round.svg" height="55" width="55" alt="logo" />
+                <div className="absolute inset-0 flex items-center justify-center flex-col space-y-1">
+                  <p className="text-white text-center leading-none -mb-px" style={{ fontSize: 9 }}>
+                    Round
+                  </p>
+                  <p className="text-center text-white text-sm leading-none">
+                    {currentRound.order}/{totalRounds}
+                  </p>
+                </div>
               </div>
-            </div>
-          ) : null}
-        </Navigation>
+            ) : null}
+          </div>
+          {currentRound && (
+            <button className="text-gray-400 hover:text-white" onClick={onExit}>
+              Exit
+            </button>
+          )}
+        </nav>
       ) : null}
-      <MainContainer>{children}</MainContainer>
-    </div>
+      <div className="h-full px-4 overflow-y-scroll">{children}</div>
+    </Container>
   );
 };
 
